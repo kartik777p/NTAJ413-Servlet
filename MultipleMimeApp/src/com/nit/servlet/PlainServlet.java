7ff217dec43e570d54a7398173c297b1c815a66a
Kp-Kartik <Kp-Kartik@DESKTOP-RPIFCKD>
2021-02-23 14:08:45 +0530

    multiple mime type basedApp

---
+++ MultipleMimeApp/src/com/nit/servlet/PlainServlet.java
@@ -0,0 +1,29 @@
+package com.nit.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class PlainServlet  extends HttpServlet {
+	@Override
+	public void service(HttpServletRequest req, HttpServletResponse res)throws ServletException,IOException{
+		PrintWriter pw=null;
+		//set responseContent type
+		res.setContentType("text/plain");
+		//get printWriter
+		pw=res.getWriter();
+		//write logic
+		pw.println("<table border='1'>");
+		pw.println("<tr><th>Player</th> <th>Role</th></tr>");
+		pw.println("<tr><td>Dhoni</td> <td>Captain</td></tr>");
+		pw.println("<tr><td>Gambhir</td> <td>Vice-Captain</td></tr>");
+		pw.println("</table>");
+		//close stream
+		pw.close();
+	}
+
+}
